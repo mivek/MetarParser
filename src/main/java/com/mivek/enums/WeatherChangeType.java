7e6f64b69a46e4160b7b6dfa07b10baf2c280b33
jk KPADEY <kpadeyjk@gmail.com>
2018-04-14 12:46:17 +0200

    Fix checkstyle in enums package

--- src/main/java/com/mivek/enums/WeatherChangeType.java
+++ src/main/java/com/mivek/enums/WeatherChangeType.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.mivek.enums;
 
 import i18n.Messages;
@@ -9,19 +6,43 @@ import i18n.Messages;
  * @author mivek
  *
  */
-public enum WeatherChangeType{
-	FM("FM", Messages.FROM), 
-	BECMG("BECMG", Messages.BECMG), 
+public enum WeatherChangeType {
+	/**
+	 * From enumeration.
+	 */
+	FM("FM", Messages.FROM),
+	/**
+	 * Becoming enumeration.
+	 */
+	BECMG("BECMG", Messages.BECMG),
+	/**
+	 * Tempo enumeration.
+	 */
 	TEMPO("TEMPO", Messages.TEMPO);
-	
-	private String fShortcut="";
-	private String fName ="";
-	
+	/**
+	 * Shortcut attribute.
+	 */
+	private String fShortcut = "";
+	/**
+	 * Name of the enumeration.
+	 */
+	private String fName = "";
+
+	/**
+	 * Constructor.
+	 * @param pShortcut
+	 *            the shortcut of the enumeration
+	 * @param pName
+	 *            the name of the enumeration
+	 */
 	private WeatherChangeType(final String pShortcut, final String pName) {
 		fShortcut = pShortcut;
 		fName = pName;
 	}
 
+	/**
+	 * @return the shortcut.
+	 */
 	public String getShortcut() {
 		return fShortcut;
 	}
---
+++ src/main/java/com/mivek/enums/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains enumerations of the application.
+ * @author mivek
+ */
+package com.mivek.enums;
