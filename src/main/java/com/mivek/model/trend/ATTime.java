4600d876602cd0ebc84a4a0ddbc64119ccdc517a
jk KPADEY <kpadeyjk@gmail.com>
2018-05-07 20:43:31 +0200

    Added classes related to time in metar trends.

---
+++ src/main/java/com/mivek/model/trend/ATTime.java
@@ -0,0 +1,18 @@
+package com.mivek.model.trend;
+
+import com.mivek.enums.TimeIndicator;
+
+/**
+ * This class represents the AT part of the trend in a metar.
+ * @author mivek
+ */
+public class ATTime extends AbstractMetarTrendTime {
+
+	/**
+	 * Constructor.
+	 */
+	public ATTime() {
+		super(TimeIndicator.AT);
+	}
+
+}
