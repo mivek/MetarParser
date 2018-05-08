701649491e14ae0f20d7729a109cdf3b66b028e2
jk KPADEY <kpadeyjk@gmail.com>
2018-05-08 12:11:43 +0200

    Added hierarchy classes for trends.
    AbstractTrend: the abstract super class. Contains wind, clouds,
    weatherConditions and visibility.
    
    AbstractMetarTrend: Subclass of AbstractTrend. Abstract class for metar
    trends. Contains attributes for the time of the trend.
    
    BECMGMetarTrend: Subclass of AbstractMetarTrend. Represents the BECMG
    part.
    
    TEMPOMetarTrend: Subclass of AbstractMetarTrend. Represents the Tempo part.

---
+++ src/main/java/com/mivek/model/trend/AbstractMetarTrend.java
@@ -0,0 +1,68 @@
+package com.mivek.model.trend;
+
+import com.mivek.enums.WeatherChangeType;
+import com.mivek.model.Metar;
+
+/**
+ * @author mivek
+ *
+ */
+public class AbstractMetarTrend extends AbstractTrend<Metar> {
+	/**
+	 * At attribute (can be null).
+	 */
+	private ATTime fAt;
+	/**
+	 * From attribute (can be null).
+	 */
+	private FMTime fFM;
+	/**
+	 * TILL attribute (can be null).
+	 */
+	private TLTime fTL;
+
+	/**
+	 * Constructor.
+	 * @param pType the WeatherChangeType to set.
+	 */
+	protected AbstractMetarTrend(final WeatherChangeType pType) {
+		super(pType);
+	}
+
+	/**
+	 * @return the at
+	 */
+	public final ATTime getAt() {
+		return fAt;
+	}
+	/**
+	 * @param pAt the at to set
+	 */
+	public void setAt(final ATTime pAt) {
+		fAt = pAt;
+	}
+	/**
+	 * @return the fM
+	 */
+	public FMTime getFM() {
+		return fFM;
+	}
+	/**
+	 * @param pFM the fM to set
+	 */
+	public void setFM(final FMTime pFM) {
+		fFM = pFM;
+	}
+	/**
+	 * @return the tL
+	 */
+	public final TLTime getTL() {
+		return fTL;
+	}
+	/**
+	 * @param pTL the tL to set
+	 */
+	public final void setTL(final TLTime pTL) {
+		fTL = pTL;
+	}
+}
---
+++ src/main/java/com/mivek/model/trend/BECMGMetarTrend.java
@@ -0,0 +1,18 @@
+package com.mivek.model.trend;
+
+import com.mivek.enums.WeatherChangeType;
+
+/**
+ * BECMG class for metar.
+ * @author mivek
+ */
+public final class BECMGMetarTrend extends AbstractMetarTrend {
+
+	/**
+	 * Constructor.
+	 */
+	public BECMGMetarTrend() {
+		super(WeatherChangeType.BECMG);
+	}
+
+}
