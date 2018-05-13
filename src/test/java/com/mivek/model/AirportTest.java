c5d1a205f899adeb118d8d515d8bcc012f402f4b
jk KPADEY <kpadeyjk@gmail.com>
2018-05-13 15:33:40 +0200

    fix tests

--- src/test/java/com/mivek/model/AirportTest.java
+++ src/test/java/com/mivek/model/AirportTest.java
@@ -1,32 +1,32 @@
 package com.mivek.model;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class AirportTest {
-	Airport sut;
-	
-	@Before
-	public void init() {
-		sut = new Airport();
-	}
-	
-	@Test
-	public void testEqual() {
-		sut.setIcao("111");
-		
-		Airport other1 = new Airport();
-		other1.setIcao("111");
-		
-		Airport other2 = new Airport();
-		other2.setIcao("498");
-		
-		assertTrue(sut.equals(sut));
-		assertTrue(sut.equals(other1));
-		assertFalse(sut.equals(other2));
-		assertFalse(sut.equals(null));
-	}	
+    private Airport sut;
+
+    @Before
+    public void init() {
+        sut = new Airport();
+    }
+
+    @Test
+    public void testEqual() {
+        sut.setIcao("111");
+
+        Airport other1 = new Airport();
+        other1.setIcao("111");
+
+        Airport other2 = new Airport();
+        other2.setIcao("498");
+
+        assertEquals(sut, sut);
+        assertEquals(other1, sut);
+        assertNotEquals(other2, sut);
+        assertNotEquals(null, sut);
+    }
 }
