95655c756ce32fc011024ec74dd41062898e4219
jk KPADEY <kpadeyjk@gmail.com>
2018-05-13 15:22:46 +0200

    fix indent in exception and facade package.

--- src/main/java/com/mivek/facade/AbstractWeatherCodeFacade.java
+++ src/main/java/com/mivek/facade/AbstractWeatherCodeFacade.java
@@ -9,23 +9,29 @@ import com.mivek.parser.AbstractParser;
  * @param <T> a concrete sub-class of {@link AbstractWeatherCode}.
  */
 public abstract class AbstractWeatherCodeFacade<T extends AbstractWeatherCode> implements IWeatherCodeFacade<T> {
+    /**
+     * Const for icao length.
+     */
+    public static final int ICAO = 4;
 
-	/**
-	 * Const for icao length.
-	 */
-	public static final int ICAO = 4;
+    /**
+     * The parser.
+     */
+    private AbstractParser<T> fParser;
 
-	/**
-	 * The parser.
-	 */
-	private AbstractParser<T> fParser;
+    /**
+     * Constructor.
+     * @param pParser the parser to set.
+     */
+    protected AbstractWeatherCodeFacade(final AbstractParser<T> pParser) {
+        fParser = pParser;
+    }
 
-	/**
-	 * Constructor.
-	 * @param pParser the parser to set.
-	 */
-	protected AbstractWeatherCodeFacade(final AbstractParser<T> pParser) {
-		fParser = pParser;
-	}
+    /**
+     * @return the parser.
+     */
+    protected AbstractParser<T> getParser() {
+        return fParser;
+    }
 
 }
