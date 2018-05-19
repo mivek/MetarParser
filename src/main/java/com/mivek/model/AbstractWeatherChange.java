43ae19fa74e0babf0cbfe7769b84a9ed43e7659d
jk KPADEY <kpadeyjk@gmail.com>
2018-05-19 11:57:03 +0200

    Made AbstractWeatherChange class child of AbstractTrend

--- src/main/java/com/mivek/model/AbstractWeatherChange.java
+++ src/main/java/com/mivek/model/AbstractWeatherChange.java
@@ -1,17 +1,14 @@
 package com.mivek.model;
 
 import com.mivek.enums.WeatherChangeType;
+import com.mivek.model.trend.AbstractTrend;
 
 /**
  * Class representing a weather change.
  * @param <T> a concrete subclass of {@link AbstractValidity}
  * @author mivek
  */
-public abstract class AbstractWeatherChange<T extends AbstractValidity> extends AbstractWeatherContainer {
-    /**
-     * the type of change.
-     */
-    private WeatherChangeType fType;
+public abstract class AbstractWeatherChange<T extends AbstractValidity> extends AbstractTrend {
     /**
      * The validity of the change.
      */
@@ -34,8 +31,7 @@ public abstract class AbstractWeatherChange<T extends AbstractValidity> extends 
      * @param pType the type to set.
      */
     protected AbstractWeatherChange(final WeatherChangeType pType) {
-        super();
-        fType = pType;
+        super(pType);
     }
 
     /**
@@ -52,12 +48,6 @@ public abstract class AbstractWeatherChange<T extends AbstractValidity> extends 
         fValidity = pValidity;
     }
 
-    /**
-     * @return the type of change.
-     */
-    public WeatherChangeType geType() {
-        return fType;
-    }
 
     /**
      * @return the probability
