deb5b651ea18f773922f0881ec63eeb6ea438ee3
jk KPADEY <kpadeyjk@gmail.com>
2018-11-17 13:47:52 +0100

    Removed ignore tests

--- src/test/java/com/mivek/facade/TAFFacadeTest.java
+++ src/test/java/com/mivek/facade/TAFFacadeTest.java
@@ -1,7 +1,10 @@
 package com.mivek.facade;
 
+import org.junit.Ignore;
+
 import com.mivek.model.TAF;
 
+@Ignore
 public class TAFFacadeTest extends AbstractWeatherCodeFacadeTest<TAF> {
 
     private TAFFacade sut = TAFFacade.getInstance();
