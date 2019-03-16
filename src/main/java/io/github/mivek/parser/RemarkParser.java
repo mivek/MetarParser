796dce7de7da5134203bb3a70ddc70869e6beb8c
jk KPADEY <kpadeyjk@gmail.com>
2019-03-16 22:43:37 +0100

    Parse remark Wind peak
    Added english and french translations

--- src/main/java/io/github/mivek/parser/RemarkParser.java
+++ src/main/java/io/github/mivek/parser/RemarkParser.java
@@ -1,5 +1,6 @@
 package io.github.mivek.parser;
 
+import java.text.MessageFormat;
 import java.util.regex.Pattern;
 
 import io.github.mivek.utils.Regex;
@@ -16,6 +17,8 @@ public final class RemarkParser {
     private static final Pattern AO1 = Pattern.compile("^AO1");
     /** Automated station with precipitation. */
     private static final Pattern AO2 = Pattern.compile("^AO2");
+    /**Wind peak pattern.*/
+    private static final Pattern WIND_PEAK = Pattern.compile("^PK WND (\\d{3})(\\d{2,3})\\/(\\d{2})?(\\d{2})");
 
     /***
      * Private constructor.
@@ -32,11 +35,15 @@ public final class RemarkParser {
         StringBuilder sb = new StringBuilder();
         while (!rmk.equals("")) {
             if (Regex.find(AO1, rmk)) {
-                sb.append(Messages.getInstance().getString("Remark.AO1"));
+                sb.append(Messages.getInstance().getString("Remark.AO1")).append(" ");
                 rmk = rmk.replaceAll(AO1.pattern(), "").trim();
             } else if (Regex.find(AO2, rmk)) {
-                sb.append(Messages.getInstance().getString("Remark.AO2"));
+                sb.append(Messages.getInstance().getString("Remark.AO2")).append(" ");
                 rmk = rmk.replaceAll(AO2.pattern(), "").trim();
+            } else if (Regex.find(WIND_PEAK, rmk)) {
+                String[] elmts = Regex.pregMatch(WIND_PEAK, rmk);
+                sb.append(MessageFormat.format(Messages.getInstance().getString("Remark.PeakWind"), elmts[1], elmts[2], elmts[3] == null ? "" : elmts[3], elmts[4]));
+                rmk = rmk.replaceAll(WIND_PEAK.pattern(), "").trim();
             } else {
                 String[] strSlit = rmk.split(" ", 2);
                 sb.append(strSlit[0]);
