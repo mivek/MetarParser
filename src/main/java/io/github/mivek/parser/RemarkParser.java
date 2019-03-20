df7ae7a0dc93b79b76c8248219df6fcb16ac6d32
jk KPADEY <kpadeyjk@gmail.com>
2019-03-20 21:36:01 +0100

    Use replaceFirst instead of replaceAll

--- src/main/java/io/github/mivek/parser/RemarkParser.java
+++ src/main/java/io/github/mivek/parser/RemarkParser.java
@@ -55,62 +55,62 @@ public final class RemarkParser {
         while (!rmk.equals("")) {
             if (Regex.find(AO1, rmk)) {
                 sb.append(Messages.getInstance().getString("Remark.AO1")).append(" ");
-                rmk = rmk.replaceAll(AO1.pattern(), "").trim();
+                rmk = rmk.replaceFirst(AO1.pattern(), "").trim();
             } else if (Regex.find(AO2, rmk)) {
                 sb.append(Messages.getInstance().getString("Remark.AO2")).append(" ");
-                rmk = rmk.replaceAll(AO2.pattern(), "").trim();
+                rmk = rmk.replaceFirst(AO2.pattern(), "").trim();
             } else if (Regex.find(WIND_PEAK, rmk)) {
                 String[] windPeakParts = Regex.pregMatch(WIND_PEAK, rmk);
                 sb.append(Messages.getInstance().getString("Remark.PeakWind", windPeakParts[1], windPeakParts[2], windPeakParts[3] == null ? "" : windPeakParts[3], windPeakParts[4]));
                 sb.append(" ");
-                rmk = rmk.replaceAll(WIND_PEAK.pattern(), "").trim();
+                rmk = rmk.replaceFirst(WIND_PEAK.pattern(), "").trim();
             } else if (Regex.find(WIND_SHIFT_FROPA, rmk)) {
                 String[] windShiftParts = Regex.pregMatch(WIND_SHIFT_FROPA, rmk);
                 sb.append(Messages.getInstance().getString("Remark.WindShift.FROPA", windShiftParts[1] == null ? "" : windShiftParts[1], windShiftParts[2]));
                 sb.append(" ");
-                rmk = rmk.replaceAll(WIND_SHIFT_FROPA.pattern(), "");
+                rmk = rmk.replaceFirst(WIND_SHIFT_FROPA.pattern(), "");
             } else if (Regex.find(WIND_SHIFT, rmk)) {
                 String[] windShiftParts = Regex.pregMatch(WIND_SHIFT, rmk);
                 sb.append(Messages.getInstance().getString("Remark.WindShift", windShiftParts[1] == null ? "" : windShiftParts[1], windShiftParts[2]));
                 sb.append(" ");
-                rmk = rmk.replaceAll(WIND_SHIFT.pattern(), "").trim();
+                rmk = rmk.replaceFirst(WIND_SHIFT.pattern(), "").trim();
             } else if (Regex.find(TOWER_VISIBILITY, rmk)) {
                 String[] towerVisibilityParts = Regex.pregMatch(TOWER_VISIBILITY, rmk);
                 sb.append(Messages.getInstance().getString("Remark.TowerVisibility", towerVisibilityParts[1])).append(" ");
-                rmk = rmk.replaceAll(TOWER_VISIBILITY.pattern(), "").trim();
+                rmk = rmk.replaceFirst(TOWER_VISIBILITY.pattern(), "").trim();
             } else if (Regex.find(SURFACE_VISIBILITY, rmk)) {
                 String[] surfaceVisivilityParts = Regex.pregMatch(SURFACE_VISIBILITY, rmk);
                 sb.append(Messages.getInstance().getString("Remark.SurfaceVisibility", surfaceVisivilityParts[1])).append(" ");
-                rmk = rmk.replaceAll(SURFACE_VISIBILITY.pattern(), "").trim();
+                rmk = rmk.replaceFirst(SURFACE_VISIBILITY.pattern(), "").trim();
             } else if (Regex.find(VARIABLE_PREVAILING_VISIBILITY, rmk)) {
                 String[] visibilityParts = Regex.pregMatch(VARIABLE_PREVAILING_VISIBILITY, rmk);
                 sb.append(Messages.getInstance().getString("Remark.VariablePrevailingVisibility", visibilityParts[1], visibilityParts[5])).append(" ");
-                rmk = rmk.replaceAll(VARIABLE_PREVAILING_VISIBILITY.pattern(), "").trim();
+                rmk = rmk.replaceFirst(VARIABLE_PREVAILING_VISIBILITY.pattern(), "").trim();
             } else if (Regex.find(SECTOR_VISIBILITY, rmk)) {
                 String[] sectorVisibilityParts = Regex.pregMatch(SECTOR_VISIBILITY, rmk);
                 sb.append(Messages.getInstance().getString("Remark.SectorVisibility", Messages.getInstance().getString("Converter." + sectorVisibilityParts[1]), sectorVisibilityParts[2]))
                 .append(" ");
-                rmk = rmk.replaceAll(SECTOR_VISIBILITY.pattern(), "").trim();
+                rmk = rmk.replaceFirst(SECTOR_VISIBILITY.pattern(), "").trim();
             } else if (Regex.find(SECOND_LOCATION_VISIBILITY, rmk)) {
                 String[] secondLocationVisibilityParts = Regex.pregMatch(SECOND_LOCATION_VISIBILITY, rmk);
                 sb.append(Messages.getInstance().getString("Remark.SecondLocationVisibility", secondLocationVisibilityParts[1], secondLocationVisibilityParts[5])).append(" ");
-                rmk = rmk.replaceAll(SECOND_LOCATION_VISIBILITY.pattern(), "").trim();
+                rmk = rmk.replaceFirst(SECOND_LOCATION_VISIBILITY.pattern(), "").trim();
             } else if (Regex.find(TORNADIC_ACTIVITY_BEG_END, rmk)) {
                 String[] tornadicParts = Regex.pregMatch(TORNADIC_ACTIVITY_BEG_END, rmk);
                 sb.append(Messages.getInstance().getString("Remark.TornadicActivity.BegEnd", Messages.getInstance().getString("Remark." + tornadicParts[1].replace(" ", "")),
                         tornadicParts[3] == null ? "" : tornadicParts[3], tornadicParts[4], tornadicParts[6] == null ? "" : tornadicParts[6], tornadicParts[7], tornadicParts[9],
                                 Messages.getInstance().getString("Converter." + tornadicParts[10]))).append(" ");
-                rmk = rmk.replaceAll(TORNADIC_ACTIVITY_BEG_END.pattern(), "").trim();
+                rmk = rmk.replaceFirst(TORNADIC_ACTIVITY_BEG_END.pattern(), "").trim();
             } else if (Regex.find(TORNADIC_ACTIVITY_BEGINNING, rmk)) {
                 String[] tornadicParts = Regex.pregMatch(TORNADIC_ACTIVITY_BEGINNING, rmk);
                 sb.append(Messages.getInstance().getString("Remark.TornadicActivity.Beginning", Messages.getInstance().getString("Remark." + tornadicParts[1].replace(" ", "")),
                         tornadicParts[3] == null ? "" : tornadicParts[3], tornadicParts[4], tornadicParts[6], Messages.getInstance().getString("Converter." + tornadicParts[7]))).append(" ");
-                rmk = rmk.replaceAll(TORNADIC_ACTIVITY_BEGINNING.pattern(), "").trim();
+                rmk = rmk.replaceFirst(TORNADIC_ACTIVITY_BEGINNING.pattern(), "").trim();
             } else if (Regex.find(TORNADIC_ACTIVITY_ENDING, rmk)) {
                 String[] tornadicParts = Regex.pregMatch(TORNADIC_ACTIVITY_ENDING, rmk);
                 sb.append(Messages.getInstance().getString("Remark.TornadicActivity.Ending", Messages.getInstance().getString("Remark." + tornadicParts[1].replace(" ", "")),
                         tornadicParts[3] == null ? "" : tornadicParts[3], tornadicParts[4], tornadicParts[6], Messages.getInstance().getString("Converter." + tornadicParts[7]))).append(" ");
-                rmk = rmk.replaceAll(TORNADIC_ACTIVITY_ENDING.pattern(), "").trim();
+                rmk = rmk.replaceFirst(TORNADIC_ACTIVITY_ENDING.pattern(), "").trim();
             } else {
                 String[] strSlit = rmk.split(" ", 2);
                 sb.append(strSlit[0]);
