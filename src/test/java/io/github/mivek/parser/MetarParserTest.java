ae3b137f14fea034363d070e507076a354420b7c
jk KPADEY <kpadeyjk@gmail.com>
2019-03-24 14:24:16 +0100

    fix tests

--- src/test/java/io/github/mivek/parser/MetarParserTest.java
+++ src/test/java/io/github/mivek/parser/MetarParserTest.java
@@ -363,6 +363,6 @@ public class MetarParserTest extends AbstractParserTest<Metar> {
         assertNotNull(m);
         assertNotNull(m.getVisibility());
         assertEquals("1 3/4SM", m.getVisibility().getMainVisibility());
-        assertThat(m.getRemark(), containsString("SF5NS3 SLP134"));
+        assertThat(m.getRemark(), containsString("SF5NS3 " + Messages.getInstance().getString("Remark.Sea.Level.Pressure", "1013.4")));
     }
 }
