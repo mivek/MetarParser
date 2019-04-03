be27346fe8f0b551e024afa4f675321280b35558
jk KPADEY <kpadeyjk@gmail.com>
2019-04-03 20:54:10 +0200

    Added scripts for install and deploy

---
+++ scripts/script.sh
@@ -0,0 +1,9 @@
+if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];then
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar --settings ../.settings.xml -P release,ossrh
+    else
+        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
+    fi
+else
+    mvn install
+fi
