51617ecb1504691026d108bb97504b55ee22fe5d
jk KPADEY <kpadeyjk@gmail.com>
2019-04-04 20:11:37 +0200

    Added url for sonar

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,8 +1,8 @@
 if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];then
     if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar --settings ../.settings.xml -P release,ossrh
+        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io --settings ../.settings.xml -P release,ossrh
     else
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
+        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.host.url=https://sonarcloud.io
     fi
 else
     mvn install
