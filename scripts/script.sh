3a3adaeb99bc8d29cba46634a634051b3d6ed370
jk KPADEY <kpadeyjk@gmail.com>
2019-04-05 20:27:31 +0200

    update script

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,9 +1,5 @@
-if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];then
-    if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar --settings ../.settings.xml -P release,ossrh
-    else
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.branch.name=$TRAVIS_PULL_REQUEST_BRANCH
-    fi
+if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];then
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar --settings ../.settings.xml -P release,ossrh
 else
     mvn install
 fi
