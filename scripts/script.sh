b2f10502005e4999598d890aa5b743738022bd3d
jk KPADEY <kpadeyjk@gmail.com>
2019-04-05 20:13:30 +0200

    added branchname for sonar

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,7 +2,7 @@ if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];then
     if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
         mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar --settings ../.settings.xml -P release,ossrh
     else
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
+        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.branch.name=$TRAVIS_PULL_REQUEST_BRANCH
     fi
 else
     mvn install
