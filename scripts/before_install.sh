570e224fa339a1278ff3802be8eeee6baa46306d
jk KPADEY <kpadeyjk@gmail.com>
2019-04-09 20:32:09 +0200

    fix deploy scripts
    Signed-off-by: jk KPADEY <kpadeyjk@gmail.com>

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];then
     echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
     echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
