9888e5a4f79216db6355069d4c09227868f4e721
jk KPADEY <kpadeyjk@gmail.com>
2019-05-04 16:10:00 +0200

    Fixed the github release process

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -5,6 +5,5 @@ if  [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 	git config --local user.name $NAME
     git config --local user.email $EMAIL
-    VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-    git tag ${VERSION}
+    git tag $1
 fi
