7d924a8796678786936d5717309e64edee7d32a6
jk KPADEY <kpadeyjk@gmail.com>
2019-05-08 13:31:36 +0200

    Fix script to filter branch

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];then
+if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ];then
     echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
     echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
 fi
--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];then
-    if [ "$TRAVIS_PULL_REQUEST" = "false" ];then
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ];then
         mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar --settings .settings.xml -P release,ossrh
     else
         mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
