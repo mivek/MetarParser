a1c4ec1b67889b31bb8ea9a194ba39cf2124f3e5
jk KPADEY <kpadeyjk@gmail.com>
2019-06-15 11:55:40 +0200

    Updated scripts so that pom is automatically updated.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
+isPRMerged=false
+
 if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ];then
     echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
     echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
 fi
 
+if [[ $1 =~ (Merge pull request.*bugfix) ]] ; then
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0 versions:commit
+    $isPRMerged=true
+elif [[ $1 =~ (Merge pull request.*feature) ]] ; then
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0 versions:commit
+    $isPRMerged=true
+fi
+
+if [[ $isPRMerged = true ]] ; then
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+    git add pom.xml
+    git commit -m "Bump pom"
+    git push https://${TRAVIS_GIT_USER}@github.com/mivek/metarParser.git HEAD:master
+fi
--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 if  [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];then
-    if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ];then
+    # The merge will not be deployed but the build after the bump pom will be deployed to maven central
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ] && ! [[ $1 =~ (Merge pull request.*bugfix) ]] && ! [[ $i =~ (Merge pull request.*feature) ]] ;then
         mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar --settings .settings.xml -P release,ossrh
     else
         mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
