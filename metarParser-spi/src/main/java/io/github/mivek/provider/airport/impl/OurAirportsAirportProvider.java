c508e14822b8d09fb18654ba911ec43119db1e6e
Jk <kpadeyjk@gmail.com>
2022-12-06 22:15:05 +0100

    Replaced opencsv with apache-commons-csv
    Updated dependencies

--- metarParser-spi/src/main/java/io/github/mivek/provider/airport/impl/OurAirportsAirportProvider.java
+++ metarParser-spi/src/main/java/io/github/mivek/provider/airport/impl/OurAirportsAirportProvider.java
@@ -1,15 +1,8 @@
 package io.github.mivek.provider.airport.impl;
 
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.exceptions.CsvValidationException;
 import io.github.mivek.model.Airport;
 import io.github.mivek.model.Country;
 import io.github.mivek.provider.airport.AirportProvider;
-import org.apache.commons.lang3.math.NumberUtils;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -22,6 +15,10 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang3.math.NumberUtils;
 
 /**
  * Implementation of the AirportProvider based on ourAirports.
@@ -38,20 +35,19 @@ public final class OurAirportsAirportProvider implements AirportProvider {
     private final Map<String, Country> countries;
     /** Map of airports. */
     private final Map<String, Airport> airports;
-    /** Common CSV Parser. */
-    private final CSVParser parser;
+    /** Common CSV format. */
+    private final CSVFormat csvFormat;
 
     /**
      * Default constructor.
      *
-     * @throws CsvValidationException when the parsing of the file fails
      * @throws IOException            when network error
      * @throws URISyntaxException     when the URI is invalid
      */
-    public OurAirportsAirportProvider() throws CsvValidationException, IOException, URISyntaxException, InterruptedException {
+    public OurAirportsAirportProvider() throws IOException, URISyntaxException, InterruptedException {
         countries = new HashMap<>();
         airports = new HashMap<>();
-        parser = new CSVParserBuilder().withIgnoreQuotations(true).build();
+        csvFormat = CSVFormat.RFC4180;
         buildCountries();
         buildAirport();
     }
@@ -59,11 +55,10 @@ public final class OurAirportsAirportProvider implements AirportProvider {
     /**
      * Connects to the countries list and build a map of {@link Country} with the name as key.
      *
-     * @throws CsvValidationException when the parsing of the file fails
      * @throws IOException            when network error
      * @throws URISyntaxException     when the URI is invalid
      */
-    public void buildCountries() throws URISyntaxException, IOException, CsvValidationException, InterruptedException {
+    public void buildCountries() throws URISyntaxException, IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(COUNTRIES_URI))
                 .GET()
@@ -74,12 +69,11 @@ public final class OurAirportsAirportProvider implements AirportProvider {
         HttpResponse<InputStream> response = HttpClient.newBuilder()
                 .build()
                 .send(request, HttpResponse.BodyHandlers.ofInputStream());
-        try (CSVReader reader = new CSVReaderBuilder(new InputStreamReader(response.body(), StandardCharsets.UTF_8)).withCSVParser(parser).withSkipLines(1).build()) {
-            String[] line;
-            while ((line = reader.readNext()) != null) {
+        try (CSVParser parser = csvFormat.parse(new InputStreamReader(response.body(), StandardCharsets.UTF_8))) {
+            for (CSVRecord line: parser) {
                 Country c = new Country();
-                c.setName(line[2]);
-                countries.put(line[1], c);
+                c.setName(line.get(2));
+                countries.put(line.get(1), c);
             }
         }
     }
@@ -87,11 +81,10 @@ public final class OurAirportsAirportProvider implements AirportProvider {
     /**
      * Connects to the airports list and build a map of {@link Airport} with the name as key.
      *
-     * @throws CsvValidationException when the parsing of the file fails
      * @throws IOException            when network error
      * @throws URISyntaxException     when the URI is invalid
      */
-    public void buildAirport() throws URISyntaxException, IOException, CsvValidationException, InterruptedException {
+    public void buildAirport() throws URISyntaxException, IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(AIRPORT_URI))
                 .GET()
@@ -102,19 +95,17 @@ public final class OurAirportsAirportProvider implements AirportProvider {
         HttpResponse<InputStream> response = HttpClient.newBuilder()
                 .build()
                 .send(request, HttpResponse.BodyHandlers.ofInputStream());
-        try (CSVReader reader = new CSVReaderBuilder(new InputStreamReader(response.body(), StandardCharsets.UTF_8)).withCSVParser(parser).withSkipLines(1).build()) {
-            String[] line;
-
-            while ((line = reader.readNext()) != null) {
+        try (CSVParser parser = csvFormat.parse(new InputStreamReader(response.body(), StandardCharsets.UTF_8))) {
+            for (CSVRecord line : parser) {
                 Airport airport = new Airport();
-                airport.setIcao(line[1]);
-                airport.setName(line[3]);
-                airport.setLatitude(NumberUtils.toDouble(line[4], 0));
-                airport.setLongitude(NumberUtils.toDouble(line[5], 0));
-                airport.setAltitude(NumberUtils.toInt(line[6], 0));
-                airport.setCountry(countries.get(line[8]));
-                airport.setCity(line[10]);
-                airport.setIata(line[13]);
+                airport.setIcao(line.get(1));
+                airport.setName(line.get(3));
+                airport.setLatitude(NumberUtils.toDouble(line.get(4), 0));
+                airport.setLongitude(NumberUtils.toDouble(line.get(5), 0));
+                airport.setAltitude(NumberUtils.toInt(line.get(6), 0));
+                airport.setCountry(countries.get(line.get(8)));
+                airport.setCity(line.get(10));
+                airport.setIata(line.get(13));
                 airports.put(airport.getIcao(), airport);
             }
         }
